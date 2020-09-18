eef00f2b6933957a7968c06bb1cd9b3a3393373e
yuriap <yuriap@gmail.com>
2020-09-18 19:11:18 +0300

    Version 6.6.0.8

--- modules/core/struct/create_struct_asha.sql
+++ modules/core/struct/create_struct_asha.sql
@@ -1,26 +1,47 @@
+drop table opas_ot_ashacube_ash purge;
+drop table opas_ot_ashacube_ref purge;
+drop table opas_ot_ashacube_ranges purge;
+drop table opas_ot_ashacube purge;
+
+create table opas_ot_ashacube (
+ashacube_id                  number                                           primary key references opas_objects(obj_id) on delete cascade,
+dblink                       varchar2(128)                                    references opas_db_links (db_link_name),
+START_TIME_UTC               TIMESTAMP(0),
+END_TIME_UTC                 TIMESTAMP(0),
+STATUS                       VARCHAR2(10),
+created                      timestamp default systimestamp,
+modified                     timestamp
+);
+
 create table opas_ot_ashacube_ranges (
 asharange_id                 number                                           generated always as identity primary key,
 dblink                       varchar2(128)                                    references opas_db_links (db_link_name),
-SAMPLE_TP                    VARCHAR2(1 BYTE) check (SAMPLE_TP in ('V','A','S')), --V GV$ASH, A DBA_HIST_ASH, S - Samples of GV$SESS
-START_TIME_UTC               TIMESTAMP(3),
-END_TIME_UTC                 TIMESTAMP(3),
+SAMPLE_TP                    VARCHAR2(1 BYTE) check (SAMPLE_TP in ('V','A','S','VA')), --V GV$ASH, A DBA_HIST_ASH, VA - combined, S - Samples of GV$SESS
+START_TIME_UTC               TIMESTAMP(0),
+END_TIME_UTC                 TIMESTAMP(0),
+DBID                         NUMBER, -- $LOCAL$ with some unlocal DBID comes from AWR dump
 incarnation#                 number,
+MIN_SNAP_ID                  NUMBER,
+MAX_SNAP_ID                  NUMBER,
 STATUS                       VARCHAR2(10),
-created                      timestamp default systimestamp
+created                      timestamp default systimestamp,
+preserve_policy              varchar2(1)  default 'N' not null check (preserve_policy in ('Y','N'))
 );
 
-create table opas_ot_ashacube_range_ref (
- obj_id                      number                                 not null  references opas_objects(obj_id) on delete cascade,
- asharange_id                number                                 not null  references opas_ot_ashacube_ranges(asharange_id),
- primary key (obj_id, asharange_id)
+create index idx_opas_ot_ashac_rdbl   on opas_ot_ashacube_ranges(dblink);
+
+create table opas_ot_ashacube_ref (
+ ashacube_id                  number                                 not null  references opas_ot_ashacube(ashacube_id) on delete cascade,
+ asharange_id                 number                                 not null  references opas_ot_ashacube_ranges(asharange_id) on delete cascade,
+ primary key (ashacube_id, asharange_id)
 ) organization index;
 
 create table opas_ot_ashacube_ash (
 asharange_id                 number                                           references opas_ot_ashacube_ranges (asharange_id) on delete cascade,
 DBLINK                       varchar2(128)                                    references opas_db_links (db_link_name),
---SAMPLE_TP                    VARCHAR2(1 BYTE) check (SNAP_TP in ('V','A','S')), --V GV$ASH, A DBA_HIST_ASH, S - Samples of GV$SESS
+SAMPLE_TP                    VARCHAR2(1 BYTE) check (SAMPLE_TP in ('V','A','S')), --V GV$ASH, A DBA_HIST_ASH, S - Samples of GV$SESS
 SNAP_ID                      NUMBER,
-DBID                         NUMBER, -- $LOCAL$ with some unlocal DBID comes from AWR dump
+--DBID                         NUMBER, -- $LOCAL$ with some unlocal DBID comes from AWR dump
 INSTANCE_NUMBER              NUMBER,
 --SAMPLE_ID                    NUMBER
 SAMPLE_TIME                  TIMESTAMP(3),
@@ -147,8 +168,8 @@ SUBPARTITION BY range (SAMPLE_TIME_UTC)
 ROW STORE COMPRESS ADVANCED
 PCTFREE 0;
 
-
-create index idx_opas_ot_ashac_dbl   on opas_ot_ashacube_ash(dblink);
+create index idx_opas_ot_ashac_ar   on opas_ot_ashacube_ash(asharange_id) local;
+create index idx_opas_ot_ashac_dbl  on opas_ot_ashacube_ash(dblink) local;
 
 drop table opas_ot_tmp_gv$ash;
 create global temporary table opas_ot_tmp_gv$ash ON COMMIT DELETE ROWS as select * from opas_ot_ashacube_ash where 1=2;
